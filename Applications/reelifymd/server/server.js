9b5c3c275875e543253f0fb4b25bf8a5001793b4
jumpman-cmd <codeks7407@gmail.com>
2025-04-20 14:35:16 +0200

    Fix static path in server.js for Render deployment

--- Applications/reelifymd/server/server.js
+++ Applications/reelifymd/server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../reelifymd/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // API route example
 app.get("/api/movies", (req, res) => {
@@ -20,7 +20,7 @@ app.get("/api/movies", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../reelifymd/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
